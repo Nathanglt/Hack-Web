7e9e6a78870b4b0506ed93ca4a0017cad326fa27
Nathan GUILLET <NGUILLET>
2022-04-06 11:06:10 +0200

    en cours

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Favori;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -36,5 +37,15 @@ class HomeController extends AbstractController
         return $this->render('security\register.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    /**
+     * @Route("/favori", name="favori")
+     */
+    public function getlesfavoris(): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(Favori::class);
+        $favori = $repository->findAll();
+        return $this->render('home/viewFavori.html.twig', ['lesfavoris'=>$favori]);
     }
 }
